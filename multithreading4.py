370a896f70ff086e5bfc1efdf77a27e456909400
RahulSinghazm <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-13 12:06:17 +0530

    commit

---
+++ multithreading4.py
@@ -0,0 +1,13 @@
+import threading
+class x (threading.Thread):
+    def run(self):
+        for i in range(1,10):
+            print(i)
+class y(threading.Thread):
+    def run(self):
+        for j in range(101,201):
+            print(j)
+x1=x()
+x1.start()
+y1=y()
+y1.start()
